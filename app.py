19c18f7f2b57e32452d9347a773286c9c0c0fdd3
CaHbI4 <FILlove@mail.ru>
2022-06-15 21:55:50 +0300

    hw14

--- app.py
+++ app.py
@@ -12,9 +12,17 @@ app = Flask(__name__)
 
 @app.route("/movie/<title>/")
 def data_movie(title):
-    """ Выводит данные о фильме"""
-    post_movie = db_movie.get_name_movie(title)
+    """ поиск по названию """
+    post_movie = db_movie.all_name_movie(title)
     return jsonify(post_movie)
+
+
+@app.route("/movie/<int:year>/to/<int:years>/")
+def data_year(year, years):
+    """ поиск по диапазону лет выпуска. """
+    post_year = db_movie.search_by_years(year, years)
+    return jsonify(post_year)
+
 
 
 if __name__ == '__main__':
--- classes/movie.py
+++ classes/movie.py
@@ -1,6 +1,4 @@
 import sqlite3
-from pprint import pp
-
 
 
 class Movie:
@@ -8,23 +6,38 @@ class Movie:
     def __init__(self, DATABASE):
         self.DATABASE = DATABASE
 
-    def all_name_movie(self):
-        """ Выводит все названия фильмом  """
+    def connect_db(self):
+        """ Устанавливаем соединение с БД"""
         with sqlite3.connect(self.DATABASE) as conn:
             conn.row_factory = sqlite3.Row
-            search_name = conn.cursor()
-            search_name.execute("SELECT `title`, `country`, release_year, listed_in, description "
-                                "FROM netflix ")
-        return search_name.fetchall()
+            conn = conn.cursor()
+        return conn
 
-    def get_name_movie(self, title):
+    def all_name_movie(self, title):
         """ выводит поиск по названию фильма"""
+        db = self.connect_db()
+        db.execute("SELECT `title`, `country`, release_year, listed_in, description "
+                   "FROM netflix "
+                   "WHERE `title` != '' "
+                   "ORDER BY release_year DESC ")
         get_movies = []
-        for get_movie in self.all_name_movie():
-            if str(title).lower() in get_movie[0].lower():
+        for get_movie in db:
+            if str(title).lower() in get_movie['title'].lower():
                 get_movies.append(dict(get_movie))
         return get_movies
 
-# #
-# movie = Movie("DATABASE")
-# pp(movie.get_name_movie('27'))
+    def search_by_years(self, year, years):
+        """ Поиск по диапазону лет выпуска фильма """
+        db = self.connect_db()
+        sql = f""" SELECT `title`, release_year
+                  FROM netflix
+                  WHERE `release_year` >= {year}
+                  AND `release_year` <= {years}
+                  ORDER BY release_year DESC
+                  LIMIT 100
+            """
+        db.execute(sql)
+        get_movies = []
+        for get_movie in db:
+                get_movies.append(dict(get_movie))
+        return get_movies
